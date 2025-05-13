a83c76b8b59149d25614a460cdc9021145d71367
lucasnunessg <lucasnunespacheco@gmail.com>
2025-05-13 12:25:13 -0300

    feat: adicionando "crud" no gerenciador de tarefas

--- tarefas/gerenciador_tarefas.py
+++ tarefas/gerenciador_tarefas.py
@@ -6,3 +6,16 @@ class GerenciadorTarefas:
     new = titulo, descricao, prioridade
     self.tarefas.append(new)
     return new
+  
+  def listar_tarefas(self):
+    return self.tarefas
+  
+  def buscar_por_id(self, id):
+    for tarefa in self.tarefas:
+      if tarefa.id == id:
+        return tarefa
+      return None
+  
+  def apagar_tarefa(self):
+    delete = self.tarefas.remove()
+    return delete
